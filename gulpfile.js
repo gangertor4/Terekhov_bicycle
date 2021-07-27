bca42971fda032a5df86ea1439ca44fdf106daa2
Gang Torchinsky <tor4911@yandex.ru>
2021-07-27 22:42:54 +0300

    Подключение шрифтов, исправлений ошибок галпа

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
 gulp.task('sass', function() {
-    return gulp.src("sass/*.scss")
-        .pipe(less())
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream());
+  return gulp.src("sass/style.scss")
+      .pipe(sass())
+      .pipe(postcss([
+        autoprefixer(),
+      ]))
+      .pipe(gulp.dest("css"))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('server', gulp.series('sass', function() {
@@ -16,9 +21,11 @@ gulp.task('server', gulp.series('sass', function() {
         }
     });
 
-    gulp.watch("sass/*.scss", gulp.series('less'));
+    gulp.watch("sass/*.scss", gulp.series('sass'));
     gulp.watch("*.html").on('change', browserSync.reload);
     gulp.watch("js/*.js").on('change', browserSync.reload);
 }));
+
+
 
 gulp.task('default', gulp.series('server'));
