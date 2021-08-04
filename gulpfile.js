485e7f5118df6aaceaeb163e738f0bfa73659196
Gang Torchinsky <tor4911@yandex.ru>
2021-08-04 21:34:36 +0300

    Замена галп-файла

--- gulpfile.js
+++ gulpfile.js
@@ -17,12 +17,12 @@ const concat = require('gulp-concat');
 
 const styles = () => {
   return gulp.src("source/sass/style.scss")
-      .pipe(sass())
-      .pipe(postcss([
-        autoprefixer(),
-      ]))
-      .pipe(gulp.dest("build/css"))
-      .pipe(browserSync.stream());
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer(),
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(browserSync.stream());
 };
 
 exports.styles = styles;
@@ -30,15 +30,15 @@ exports.styles = styles;
 //Server & watcher
 
 const server = (done) => {
-browserSync.init({
-  server: {
-    baseDir: "build"
-  },
-  cors: true,
-  notify: false,
-  ui: false,
-});
-done();
+  browserSync.init({
+    server: {
+      baseDir: "build"
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
 }
 
 exports.server = server;
@@ -46,25 +46,27 @@ exports.server = server;
 // Reload
 
 const reload = done => {
-browserSync.reload();
-done();
+  browserSync.reload();
+  done();
 }
 
 // Watcher
 
 const watcher = () => {
-gulp.watch("source/sass/**/*.scss", gulp.series(stylesMin));
-gulp.watch("source/js/*.js");
-gulp.watch("source/*.html", gulp.series(html, reload));
+  gulp.watch("source/sass/**/*.scss", gulp.series(stylesMin));
+  gulp.watch("source/js/*.js");
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
 
 // WebP
 
 const createWebp = () => {
-return gulp.src("source/img/**/*.{jpg,png}")
-  .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("build/img"))
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("build/img"))
 }
 
 exports.createWebp = createWebp;
@@ -74,24 +76,28 @@ exports.createWebp = createWebp;
 const images = () => {
   return gulp.src("source/img/*.{png,jpg,svg}")
     .pipe(imagemin([
-      imagemin.mozjpeg({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("build/img"))
-  }
+}
 
 exports.images = images;
 
 const copy = (done) => {
-gulp.src([
-  "source/fonts/*.{woff2,woff}",
-  "source/img/*.{png,svg}",
-], {
-  base: "source"
-})
-  .pipe(gulp.dest("build"))
-done();
+  gulp.src([
+      "source/fonts/*.{woff2,woff}",
+      // "source/img/*.{png,svg}",
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"))
+  done();
 }
 
 exports.copy = copy;
@@ -106,56 +112,57 @@ const concatJs = () => {
 exports.concatJs = concatJs;
 
 const stylesMin = () => {
-return gulp.src("source/sass/style.scss")
-  .pipe(plumber())
-  .pipe(sourcemap.init())
-  .pipe(sass())
-  .pipe(postcss([
-    autoprefixer(),
-    csso()
-  ]))
-  .pipe(rename("style.min.css"))
-  .pipe(sourcemap.write("."))
-  .pipe(gulp.dest("build/css"))
-  .pipe(browserSync.stream());
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(browserSync.stream());
 }
 
 exports.stylesMin = stylesMin;
 
 const html = () => {
-return gulp.src("source/*.html")
-  .pipe(gulp.dest("build"));
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"));
 }
 
 
 const clean = () => {
-return del("build");
+  return del("build");
 };
 
 const build = gulp.series(
-clean,
-gulp.parallel(
-  stylesMin,
-  html,
-  copy,
-  images,
-  createWebp,
-  concatJs
-));
+  clean,
+  gulp.parallel(
+    stylesMin,
+    html,
+    copy,
+    images,
+    createWebp,
+    concatJs,
+  ));
 
 exports.build = build;
 
 
 exports.default = gulp.series(
-clean,
-gulp.parallel(
-  stylesMin,
-  html,
-  copy,
-  createWebp,
-  concatJs
-),
-gulp.series(
-  server,
-  watcher
-));
+  clean,
+  gulp.parallel(
+    stylesMin,
+    html,
+    copy,
+    images,
+    createWebp,
+    concatJs,
+  ),
+  gulp.series(
+    server,
+    watcher,
+  ));
